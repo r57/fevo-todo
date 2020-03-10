3cdde967b5795bf27027621bf917156b1e84dac1
Petr Slajchrt <slajchrtp@avast.com>
2020-03-10 16:44:47 +0100

    add schema urls and fix it

--- gateway/index.ts
+++ gateway/index.ts
@@ -12,11 +12,13 @@ import { UserServiceClient } from './src/generated/pb/user_grpc_pb';
 
     const todosService: ServiceDefinition = {
         name: 'todos',
+        url: 'local',
         typeDefs: gql(await importSchema(path.join(__dirname, "src/schema/todos.graphql"))),
     }
 
     const usersService: ServiceDefinition = {
         name: 'users',
+        url: 'local',
         typeDefs: gql(await importSchema(path.join(__dirname, "src/schema/users.graphql"))),
     }
     
@@ -28,7 +30,9 @@ import { UserServiceClient } from './src/generated/pb/user_grpc_pb';
 
     const server = new ApolloServer({ gateway, subscriptions: false });
     
-    server.listen();
+    server.listen(4001).then(({ url }) => {
+        console.log(`Server ready at ${url}`);
+    });
 
 }()).catch(error => {
     console.log(error);
